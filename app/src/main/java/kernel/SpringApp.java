bf71368df435b6fe093a1343fecb512c2cff5502
maks0 <maks0_00@inbox.ru>
2021-12-24 22:45:58 +0500

    Добавлен postgre и тестовые настройки jdbc

--- app/src/main/java/kernel/SpringApp.java
+++ app/src/main/java/kernel/SpringApp.java
@@ -2,8 +2,9 @@ package kernel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class SpringApp
 {
     public static void main(String[] args)
