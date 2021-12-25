944f6af592b6b075c3d23091971b7e0542561285
maks0 <maks0_00@inbox.ru>
2021-12-25 19:45:07 +0500

    Упрощен код product с использованием lombok

--- app/src/main/java/kernel/jpa/Product.java
+++ app/src/main/java/kernel/jpa/Product.java
@@ -1,43 +1,22 @@
 package kernel.jpa;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
 @Table(name = "product")
+@NoArgsConstructor
+@Getter
+@Setter
 public class Product implements Serializable
 {
     @Id
     @Column(name = "id", nullable = false)
     private Integer id;
-
-    public Product()
-    {
-
-    }
-
-    public Integer getId()
-    {
-        return id;
-    }
-
-    public void setId(Integer id)
-    {
-        this.id = id;
-    }
-
-    public String getName()
-    {
-        return name;
-    }
-
-    public void setName(String name)
-    {
-        this.name = name;
-    }
 
     @Column(name = "name")
     private String name;
