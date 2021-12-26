1cbd65deea2c2b1e2f07a1d9cbe3b0c34dc30735
maks0 <maks0_00@inbox.ru>
2021-12-26 15:28:20 +0500

    Добавлен тест на добавление нового объекта

--- app/src/test/java/ProductRepositoryTest.java
+++ app/src/test/java/ProductRepositoryTest.java
@@ -1,6 +1,7 @@
 import kernel.entity.Product;
 import kernel.jpa.ProductRepository;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
+import java.util.Optional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -24,6 +26,16 @@ public class ProductRepositoryTest
     public void findAllTest()
     {
         List<Product> productList = productRepository.findAll();
-        Assert.assertEquals(productList.size(),2);
+        Assert.assertEquals(productList.size(),3);
+    }
+
+    @Before
+    public void createProductTest()
+    {
+        Product product = new Product(3,"productTest");
+        productRepository.save(product);
+
+        Optional<Product> optionalProduct = productRepository.findById(3);
+        Assert.assertTrue(optionalProduct.isPresent());
     }
 }
